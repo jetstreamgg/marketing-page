9bc6ad3266b6409f85361a08e9f513a1bf1ecce0
inclinedmind <138821869+inclinedmind@users.noreply.github.com>
2026-03-02 15:50:29 -0800

    Add fifth vault

--- app/(main)/fetchData.ts
+++ app/(main)/fetchData.ts
@@ -25,7 +25,8 @@ const MORPHO_VAULT_ADDRESSES = [
   '0xf42bca228D9bd3e2F8EE65Fec3d21De1063882d4', // USDS Risk Capital
   '0x56bfa6f53669B836D1E0Dfa5e99706b12c373ecf', // USDC Risk Capital
   '0xE15fcC81118895b67b6647BBd393182dF44E11E0', // USDS Flagship
-  '0x2bD3A43863c07B6A01581FADa0E1614ca5DF0E3d' // USDT Risk Capital
+  '0x2bD3A43863c07B6A01581FADa0E1614ca5DF0E3d', // USDT Risk Capital
+  '0x23f5E9c35820f4baB695Ac1F19c203cC3f8e1e11' // USDT Savings
 ];
 
 async function fetchMorphoVaultsData(): Promise<{ vaultRate: string; vaultTvl: string }> {
